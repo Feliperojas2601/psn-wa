3ba736b19c4f4b8e5fb5e3bd287eee653d11b912
dbustos106 <dbustos@unal.edu.co>
2023-05-16 00:14:21 -0500

    solucionado sockets uwu

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: '/api/', 
-  baseUrlNotificationSocket: 'http://35.185.93.206:4401/notification', 
-  baseUrlChatSocket: 'http://35.196.125.247:4300/chat/message'
+  baseUrlNotificationSocket: 'http://35.229.89.207:4401/notification', 
+  baseUrlChatSocket: 'http://35.237.27.3:4300/chat/message'
 };
