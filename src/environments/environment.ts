f8c43bf913fc5eb8f2b803472bf65d57ebd4b8f9
dbustos106 <dbustos@unal.edu.co>
2023-05-22 00:33:36 -0500

    ips components

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: '/api/', 
-  baseUrlNotificationSocket: 'http://35.231.55.100:4401/notification', 
-  baseUrlChatSocket: 'http://34.139.160.20:4300/chat/message'
+  baseUrlNotificationSocket: 'http://34.75.81.121:4401/notification', 
+  baseUrlChatSocket: 'http://34.138.3.32:4300/chat/message'
 };
